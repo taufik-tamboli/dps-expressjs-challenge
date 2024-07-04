3d02b95e03ed8bddc4115a0964677be01c024959
Taufik <tambolitaufik98@outlook.com>
2024-07-04 14:05:50 +0200

    Initial project setup - routes directory with seperate files for projects and reports api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import projectRoutes from './routes/projects';
+import reportRoutes from './routes/reports';
 
 dotenv.config();
 
@@ -7,6 +9,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/projects', projectRoutes);
+app.use('/reports', reportRoutes);
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
